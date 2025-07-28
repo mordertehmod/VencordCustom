69f3cddd63165ea814d70b4bbaaf96c862120cd6
mordertehmod <160318353+mordertehmod@users.noreply.github.com>
2025-07-28 13:30:37 +0000

    [AlwaysAnimate] Add gradient role support

--- src/plugins/alwaysAnimate/index.ts
+++ src/plugins/alwaysAnimate/index.ts
@@ -22,7 +22,7 @@ import definePlugin from "@utils/types";
 export default definePlugin({
     name: "AlwaysAnimate",
     description: "Animates anything that can be animated",
-    authors: [Devs.FieryFlames],
+    authors: [Devs.FieryFlames, Devs.LSDZaddi],
 
     patches: [
         {
@@ -53,6 +53,19 @@ export default definePlugin({
             replacement: {
                 match: /(\.headerContent.+?guildBanner:\i,animate:)\i/,
                 replace: "$1!0"
+            }
+        },
+        {
+            find: "animateGradient:",
+            all: true,
+            noWarn: true,
+            replacement: {
+                match: /animateGradient:.+?([,}].*?\))/g,
+                replace: (m, rest) => {
+                    const destructuringMatch = rest.match(/}=.+/);
+                    if (destructuringMatch == null) return `animateGradient:!0${rest}`;
+                    return m;
+                }
             }
         }
     ]
